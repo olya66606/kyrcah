daa9f8037f75ad40fa08814e5c446f05777d8b25
olya6606 <39kukol@gmail.com>
2025-02-22 20:30:10 +0200

    info

--- app.js
+++ app.js
@@ -70,3 +70,27 @@
 
 
 
+
+
+        
+        let currentPage = 0;
+        const pages = document.querySelectorAll('.page');
+    
+        function showPage(index) {
+            pages.forEach((page, i) => {
+                page.classList.toggle('active', i === index);
+            });
+        }
+    
+        function nextPage() {
+            currentPage = (currentPage + 1) % pages.length;
+            showPage(currentPage);
+        }
+    
+        function prevPage() {
+            currentPage = (currentPage - 1 + pages.length) % pages.length;
+            showPage(currentPage);
+        }
+
+
+
