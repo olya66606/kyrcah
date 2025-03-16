aab0fc9569d97ae4e79f81e9fbf98385f261b408
olya6606 <39kukol@gmail.com>
2025-03-16 16:28:34 +0200

    otzivi

--- app.js
+++ app.js
@@ -124,3 +124,31 @@ document.addEventListener('DOMContentLoaded', function () {
     setupCarousel('.cold-drinks-card-container');
     setupCarousel('.tea-card-container');
 });
+/*_____________________________________________________________________________________________*/
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const testimonials = document.querySelectorAll('.testimonial');
+
+    function checkTestimonials() {
+        testimonials.forEach(testimonial => {
+            if (isElementInViewport(testimonial)) {
+                testimonial.classList.add('active'); 
+            }
+        });
+    }
+
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    checkTestimonials();
+
+    window.addEventListener('scroll', checkTestimonials);
+});
